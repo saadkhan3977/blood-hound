8d788aa54bcc408eaed99ce3b9f56f3f24c9913d
saadkhan3977 <xaadkhan43@gmail.com>
2024-11-14 16:53:16 +0500

    post comment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -19,7 +19,7 @@ class Comment extends Model
     // Relationship to replies
     public function replies()
     {
-        return $this->hasMany(Comment::class, 'parent_id')->with('replies.user');
+        return $this->hasMany(Comment::class, 'parent_id');
     }
 
     // Relationship to user
